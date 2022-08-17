3a1b4d64c931ea17b919f0d2e5ef26da4e801af0
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-08-17 23:45:51 +0200

    refactor(cli): migrate to clap_derive

--- src/cli.rs
+++ src/cli.rs
@@ -1,151 +1,132 @@
-use clap::{command, value_parser, Arg, ArgGroup, Command, ValueHint};
+use clap::{ArgGroup, Parser, ValueHint};
 
-#[allow(clippy::too_many_lines)]
-#[must_use]
-pub fn build() -> Command<'static> {
-    command!()
-        .arg(
-            Arg::new("influx-host")
-                .long("influx-host")
-                .env("INFLUX_HOST")
-                .value_hint(ValueHint::Url)
-                .value_name("INFLUX_HOST")
-                .value_parser(value_parser!(url::Url))
-                .takes_value(true)
-                .default_value("http://localhost:8086/")
-                .help("HTTP address of InfluxDB"),
-        )
-        .arg(
-            Arg::new("influx-token")
-                .long("influx-token")
-                .env("INFLUX_TOKEN")
-                .value_hint(ValueHint::Other)
-                .value_name("TOKEN")
-                .hide_env_values(true)
-                .takes_value(true)
-                .help("InfluxDB api token with write access"),
-        )
-        .group(ArgGroup::new("influxtarget").args(&["influx-org", "influx-database"]).required(true))
-        .arg(
-            Arg::new("influx-database")
-                .long("influx-database")
-                .env("INFLUX_DATABASE")
-                .value_hint(ValueHint::Other)
-                .value_name("STRING")
-                .takes_value(true)
-                .conflicts_with_all(&["influx-org", "influx-bucket"])
-                .help("InfluxDB 1.x database to put the value in"),
-        )
-        .arg(
-            Arg::new("influx-org")
-                .long("influx-org")
-                .env("INFLUX_ORG")
-                .value_hint(ValueHint::Other)
-                .value_name("STRING")
-                .takes_value(true)
-                .conflicts_with("influx-database")
-                .requires("influx-bucket")
-                .help("InfluxDB v2 organization"),
-        )
-        .arg(
-            Arg::new("influx-bucket")
-                .long("influx-bucket")
-                .env("INFLUX_BUCKET")
-                .value_hint(ValueHint::Other)
-                .value_name("STRING")
-                .takes_value(true)
-                .conflicts_with("influx-database")
-                .requires("influx-org")
-                .default_value("mqtt")
-                .help("InfluxDB v2 bucket to put the values in"),
-        )
-        .arg(
-            Arg::new("mqtt-broker")
-                .long("mqtt-broker")
-                .env("MQTT_BROKER")
-                .value_hint(ValueHint::Hostname)
-                .value_name("HOST")
-                .takes_value(true)
-                .default_value("localhost")
-                .help("Host on which the MQTT Broker is running"),
-        )
-        .arg(
-            Arg::new("mqtt-port")
-                .long("mqtt-port")
-                .env("MQTT_PORT")
-                .value_hint(ValueHint::Other)
-                .value_name("INT")
-                .value_parser(value_parser!(u16))
-                .takes_value(true)
-                .default_value("1883")
-                .help("Port on which the MQTT Broker is running"),
-        )
-        .arg(
-            Arg::new("mqtt-user")
-                .long("mqtt-user")
-                .env("MQTT_USERNAME")
-                .value_hint(ValueHint::Username)
-                .value_name("STRING")
-                .takes_value(true)
-                .requires("mqtt-password")
-                .help("Username to access the MQTT broker")
-                .long_help(
-                    "Username to access the MQTT broker. Anonymous access when not supplied.",
-                ),
-        )
-        .arg(
-            Arg::new("mqtt-password")
-                .long("mqtt-password")
-                .env("MQTT_PASSWORD")
-                .value_hint(ValueHint::Other)
-                .value_name("STRING")
-                .hide_env_values(true)
-                .takes_value(true)
-                .requires("mqtt-user")
-                .help("Password to access the MQTT broker")
-                .long_help(
-                    "Password to access the MQTT broker. Passing the password via command line is insecure as the password can be read from the history!",
-                ),
-        )
-        .arg(
-            Arg::new("topics")
-                .env("MQTT_TOPICS")
-                .value_hint(ValueHint::Other)
-                .value_name("TOPIC")
-                .takes_value(true)
-                .multiple_values(true)
-                .default_value("#")
-                .help("MQTT topics to subscribe"),
-        )
-        .arg(
-            Arg::new("verbose")
-                .short('v')
-                .long("verbose")
-                .help("Show more details"),
-        )
-        .arg(
-            Arg::new("buffer-amount")
-                .long("buffer-amount")
-                .value_hint(ValueHint::Other)
-                .value_name("INT")
-                .value_parser(value_parser!(usize))
-                .takes_value(true)
-                .default_value("1000")
-                .help("Send the buffer when the amount of messages is reached (or the time)"),
-        )
-        .arg(
-            Arg::new("buffer-seconds")
-                .long("buffer-seconds")
-                .value_hint(ValueHint::Other)
-                .value_name("SECONDS")
-                .value_parser(value_parser!(f32))
-                .takes_value(true)
-                .default_value("28.2")
-                .help("Send the buffer when the timeout in seconds has reached (or the amount)"),
-        )
+#[derive(Debug, Parser)]
+#[clap(about, author, version)]
+#[clap(group(
+    ArgGroup::new("influxtarget")
+        .args(&["influx-org", "influx-database"])
+        .required(true)
+))]
+pub struct Cli {
+    /// HTTP address of InfluxDB
+    #[clap(
+        long, env,
+        value_hint = ValueHint::Url,
+        default_value = "http://localhost:8086/"
+    )]
+    pub influx_host: url::Url,
+
+    /// InfluxDB api token with write access
+    #[clap(
+        long, env,
+        value_hint = ValueHint::Other,
+        value_name = "TOKEN",
+        hide_env_values = true,
+    )]
+    pub influx_token: Option<String>,
+
+    /// InfluxDB 1.x database to put the value in
+    #[clap(
+        long, env,
+        value_hint = ValueHint::Other,
+        value_name = "STRING",
+        conflicts_with_all = &["influx-org", "influx-bucket"],
+    )]
+    pub influx_database: Option<String>,
+
+    /// InfluxDB v2 organization
+    #[clap(
+        long, env,
+        value_hint = ValueHint::Other,
+        value_name = "STRING",
+        conflicts_with = "influx-database",
+    )]
+    pub influx_org: Option<String>,
+
+    /// InfluxDB v2 organization
+    #[clap(
+        long, env,
+        value_hint = ValueHint::Other,
+        value_name = "STRING",
+        conflicts_with = "influx-database",
+    )]
+    pub influx_bucket: Option<String>,
+
+    /// Host on which the MQTT Broker is running
+    #[clap(
+        long, env,
+        value_hint = ValueHint::Hostname,
+        value_name = "HOST",
+        default_value = "localhost",
+    )]
+    pub mqtt_broker: String,
+
+    /// Port on which the MQTT Broker is running
+    #[clap(
+        long, env,
+        value_hint = ValueHint::Other,
+        value_name = "INT",
+        default_value = "1883",
+    )]
+    pub mqtt_port: u16,
+
+    /// Username to access the MQTT broker.
+    ///
+    /// Anonymous access when not supplied.
+    #[clap(
+        long, env,
+        value_hint = ValueHint::Username,
+        value_name = "STRING",
+        requires = "mqtt-password",
+    )]
+    pub mqtt_user: Option<String>,
+
+    /// Password to access the MQTT broker.
+    ///
+    /// Passing the password via command line is insecure as the password can be read from the history!
+    #[clap(
+        long, env,
+        value_hint = ValueHint::Other,
+        value_name = "STRING",
+        hide_env_values = true,
+        requires = "mqtt-user",
+    )]
+    pub mqtt_password: Option<String>,
+
+    /// MQTT topics to subscribe
+    #[clap(
+        env,
+        value_hint = ValueHint::Other,
+        value_name = "TOPIC",
+        default_value = "#",
+    )]
+    pub mqtt_topics: Vec<String>,
+
+    /// Show more details
+    #[clap(short, long)]
+    pub verbose: bool,
+
+    /// Send the buffer when the amount of messages is reached (or the time)
+    #[clap(
+        long, env,
+        value_hint = ValueHint::Other,
+        value_name = "INT",
+        default_value = "1000",
+    )]
+    pub buffer_amount: usize,
+
+    /// Send the buffer when the timeout in seconds has reached (or the amount)
+    #[clap(
+        long, env,
+        value_hint = ValueHint::Other,
+        value_name = "SECONDS",
+        default_value = "28.2",
+    )]
+    pub buffer_seconds: f32,
 }
 
 #[test]
 fn verify() {
-    build().debug_assert();
+    use clap::CommandFactory;
+    Cli::command().debug_assert();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::time::Duration;
 
+use clap::Parser;
 use tokio::sync::mpsc::error::TryRecvError;
 use tokio::time::sleep;
-use url::Url;
 
 mod cli;
 mod exit_handler;
@@ -12,50 +12,28 @@ mod mqtt;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let matches = cli::build().get_matches();
+    let matches = cli::Cli::parse();
 
-    let buffer_amount = *matches.get_one::<usize>("buffer-amount").unwrap();
-    let buffer_age = matches
-        .get_one::<f32>("buffer-seconds")
-        .copied()
-        .map(Duration::from_secs_f32)
-        .unwrap();
-
-    let topics = matches
-        .get_many::<String>("topics")
-        .unwrap()
-        .map(std::string::ToString::to_string)
-        .collect();
-
-    let influx_host = matches.get_one::<Url>("influx-host").unwrap();
     let mut influxdb = influxdb::Influxdb::new(
-        influx_host,
-        matches
-            .get_one::<String>("influx-token")
-            .map(String::as_str),
-        matches
-            .get_one::<String>("influx-database")
-            .map(String::as_str),
-        matches.get_one::<String>("influx-org").map(String::as_str),
-        matches
-            .get_one::<String>("influx-bucket")
-            .map(String::as_str),
-        buffer_age,
-        buffer_amount,
+        &matches.influx_host,
+        matches.influx_token.as_deref(),
+        matches.influx_database.as_deref(),
+        matches.influx_org.as_deref(),
+        matches.influx_bucket.as_deref(),
+        Duration::from_secs_f32(matches.buffer_seconds),
+        matches.buffer_amount,
     )
     .await;
-    eprintln!("InfluxDB {} connected.", influx_host);
+    eprintln!("InfluxDB {} connected.", matches.influx_host);
 
-    let mqtt_broker = matches.get_one::<String>("mqtt-broker").unwrap();
+    let mqtt_broker = matches.mqtt_broker;
     let (client, mut receiver) = mqtt::connect(
-        mqtt_broker,
-        *matches.get_one("mqtt-port").unwrap(),
-        matches.get_one::<String>("mqtt-user").map(String::as_str),
-        matches
-            .get_one::<String>("mqtt-password")
-            .map(String::as_str),
-        topics,
-        matches.contains_id("verbose"),
+        &mqtt_broker,
+        matches.mqtt_port,
+        matches.mqtt_user.as_deref(),
+        matches.mqtt_password.as_deref(),
+        matches.mqtt_topics,
+        matches.verbose,
     )
     .await;
     eprintln!("MQTT {} connected.", mqtt_broker);
